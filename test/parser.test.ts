3fedb05c119decd67d67b9ff99aad2efc2f326f7
florida99771 <florida99771@gmail.com>
2025-06-04 18:05:26 +0800

    Fix slide parsing order and sanitize text

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from 'vitest';
+import JSZip from 'jszip';
 import { pptxToHtml } from '../src/parser/pptxParser';
 
 const sample = new Blob([], { type: 'application/vnd.openxmlformats-officedocument.presentationml.presentation' });
@@ -8,5 +9,20 @@ describe('pptxToHtml', () => {
     const container = document.createElement('div');
     await pptxToHtml({ file: sample, target: container });
     expect(container.children.length).toBe(0);
+  });
+
+  it('sanitizes slide text', async () => {
+    const zip = new JSZip();
+    const xml = `<p:sld xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main"
+      xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
+      <p:cSld><p:spTree><p:sp><p:txBody><a:p><a:r><a:t><img src=x onerror=alert(1) /></a:t></a:r></a:p></p:txBody></p:sp></p:spTree></p:cSld>
+    </p:sld>`;
+    zip.file('ppt/slides/slide1.xml', xml);
+    const blob = await zip.generateAsync({ type: 'blob' });
+    const container = document.createElement('div');
+    await pptxToHtml({ file: blob, target: container });
+    const div = container.querySelector('.ppt-slide') as HTMLDivElement;
+    expect(div.textContent).toBe('<img src=x onerror=alert(1) />');
+    expect(div.innerHTML).toBe('&lt;img src=x onerror=alert(1) /&gt;');
   });
 });
